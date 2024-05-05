4a334d0e311525f6a8951b1214dae432b073bb78
Hanse Kim <procrustes5@gmail.com>
2024-05-05 16:45:02 +0900

    post succeeded

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue';
 import { supabase } from '../../utils/supabase';
 import { MdEditor } from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
 const text = ref<string>('');
+const feeds = ref<unknown[] | null>([]);
+const user = ref();
+
+const getSession = async () => {
+  const { data, error } = await supabase.auth.getSession()
+  user.value = data;
+}
+
+const getFeeds = async () => {
+  let { data, error } = await supabase
+    .from('Feeds')
+    .select('*')
+  feeds.value = data
+}
+          
+const handleSubmit = async () => {
+  const { data, error } = await supabase
+    .from('Feeds')
+    .insert([
+      { 
+        category_id: 1,
+        title: '12',
+        user_id: user.value.session?.user.id,
+        content: text.value,
+      },
+    ])
+    .select()
+  console.log(error)
+}
 
 const handleLogin = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
@@ -12,21 +41,28 @@ const handleLogin = async () => {
   })
 }
 
+onMounted(() => {
+  getFeeds()
+  getSession()
+})
 </script>
 
 <template>
   <div class="body">
     <div class="body-header">
-      <span>안녕하세요. 개발자 블로그입니다.</span>
+      <span>Developer blog</span>
     </div>
-    <span>작성</span>
+    <span>Post the feeds</span>
     <MdEditor v-model="text" language="en-US"/>
+    <div class="btn-wrapper">
+      <div class="submit-btn" @click="handleSubmit">Submit</div>
+    </div>
     <div class="blog-feed-wrapper">
-      <span class="blog-feed-title">피드</span>
+      <span class="blog-feed-title">Feed</span>
       <div v-for="(item, index) in feeds" :key="index" class="blog-feed">
         <div class="feed-header">
-          <span class="title">{{ `제목: ${item.title}` }}</span>
-          <span class="category">{{ `카테고리: ${item.category}` }}</span>
+          <span class="title">{{ `Title: ${item.title}` }}</span>
+          <span class="category">{{ `Category: ${item.category}` }}</span>
         </div>
         <div class="feed-body">
           <p>{{ item.content }}</p>
@@ -52,6 +88,23 @@ p {
     margin-bottom: 20px;
     span {
       font-size: 18px;
+    }
+  }
+  .btn-wrapper {
+    display: flex;
+    justify-content: end;
+    margin-top: 12px;
+    .submit-btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100px;
+      height: 40px;
+      background: blueviolet;
+      border-radius: 4px;
+      color: white;
+      font-weight: 600;
+      cursor: pointer;
     }
   }
   .blog-feed-wrapper {
