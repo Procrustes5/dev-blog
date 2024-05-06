bd6a0d40c2d5102dd471e52267e04b268c494c16
Hanse Kim <procrustes5@gmail.com>
2024-05-06 19:22:57 +0900

    Header refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { supabase } from '../utils/supabase'
+import Header from '@/components/Header.vue'
 </script>
 
 <template>
   <div class="container">
-    <div class="header">
-      <div class="header-left">
-        <span class="title">Developer blog</span>
-      </div>
-      <div class="header-right"></div>
-    </div>
+    <Header></Header>
     <router-view />
   </div>
 </template>
@@ -26,21 +22,5 @@ p {
   overflow-y: auto;
   flex-direction: column;
   overflow-y: auto;
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 80px;
-    background: blueviolet;
-    padding: 10px 14px;
-    .header-left {
-      .title {
-        font-size: 20px;
-        font-weight: 600;
-        color: whitesmoke;
-      }
-    }
-  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="title">Developer blog</span>
+    </div>
+    <div class="header-right"></div>
+  </div>
+</template>
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  background: blueviolet;
+  padding: 10px 14px;
+  .header-left {
+    .title {
+      font-size: 20px;
+      font-weight: 600;
+      color: whitesmoke;
+    }
+  }
+}
+</style>
